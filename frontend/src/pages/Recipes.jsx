65105083ba02a3e6bc708d54e93c651302fbb790
Lucas Gustavo Weber <lucas.weber1@universo.univates.br>
2026-04-28 13:55:06 -0300

    correção da função de gerar pdf

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -90,11 +90,12 @@ export default function Recipes({ user, onLogout }) {
     const handleDownloadPdf = async () => {
         setGeneratingPdf(true)
         try {
-            const params = new URLSearchParams()
-            if (filterDate) params.append('date', filterDate)
-            if (filterType) params.append('type', filterType)
+            let url = '/api/recipe/read/pdf';
+            const params = new URLSearchParams();
+            if (filterType) params.append('type', filterType);
+            if (filterDate) params.append('dateTime', filterDate+'T00:00:00');
+            url += `?${params.toString()}`;
 
-            const url = `/api/recipe/read/pdf${params.toString() ? '?' + params.toString() : ''}`
 
             const res = await fetch(url)
             if (!res.ok) throw new Error('Erro ao gerar PDF')
