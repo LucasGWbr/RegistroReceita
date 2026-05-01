bb15ed21db8cea14e8a3fbfcc7999938e4f82fef
Lucas Gustavo Weber <lucao8976@gmail.com>
2026-04-30 23:04:27 -0300

    alterações de ligação front e back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-
+  // Carrega as variáveis do .env
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
+
+    // 1. Proxy para o ambiente de DESENVOLVIMENTO (npm run dev - Porta 5173)
     server: {
+      proxy: {
+        '/api': {
+          target: env.REACT_APP_API_URL,
+          changeOrigin: true,
+        }
+      }
+    },
+
+    // 2. Proxy para o ambiente de PREVIEW/BUILD (npm run preview - Porta 4173)
+    preview: {
       proxy: {
         '/api': {
           target: env.REACT_APP_API_URL,
