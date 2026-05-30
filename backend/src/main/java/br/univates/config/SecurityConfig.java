099f4bb2bd73ab084de127e43c1843be475d9025
Lucas Gustavo Weber <lucao8976@gmail.com>
2026-05-30 17:00:35 -0300

    correção de erros de project pattern

--- backend/src/main/java/br/univates/config/SecurityConfig.java
+++ backend/src/main/java/br/univates/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
